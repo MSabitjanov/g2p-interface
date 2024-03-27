9083c58b301af5133d337ad7cad1b45b6c69a121
Muzaffar <msabitjanov1@gmail.com>
2024-03-27 18:01:25 +0500

    complete input generation

--- testpage.py
+++ testpage.py
@@ -13,6 +13,9 @@ st.set_page_config(
 st.title("📄Matni IPA ko'rinishiga o'giring")
 st.markdown("<br><br>", unsafe_allow_html=True)
 
+SHOW_INPUT_TABLE = True
+SHOW_GENERATED_TABLE = False
+
 def update_session_state(key, data):
     st.session_state[key] = data
 
@@ -27,36 +30,43 @@ def get_prepared_data():
 
 def get_converted_data(data) -> tuple[bool, Union[str, list]]:
     # Making request 
-    response = requests.post(
-        'http://127.0.0.1:5000/g2p/epitran?lang_code=uzb-Latn', 
-        headers={'Content-Type': 'application/json'},
-        data=json.dumps(data))
+    try:    
+        response = requests.post(
+            'http://127.0.0.1:5000/g2p/epitran?lang_code=uzb-Latn', 
+            headers={'Content-Type': 'application/json'},
+            data=json.dumps(data))
+    except requests.exceptions.ConnectionError:
+        return False, "Tashqi xizmat ishlashida xatolik yuz berdi"
     if response.status_code == 200:
         try:
             converted_data = [obj['phonetic_form'] for obj in response.json().get('data')]
             return True, converted_data
         except:
-            return False, "Something went wrong while converting response to json"
+            return False, "Javobni o'qishda xatolik yuz berdi yoki javobni tahlil qilishda xatolik yuz berdi"
     else:
-        return False, "Something went wrong while making request to external service"
+        return False, "Tashqi xizmatdan javob olishda xatolik yuz berdi"
 
-def generate_phonemes():
+def generate_phonemes() -> bool:
     # Gets data_table from session and generates phoneme for each row
     msg = st.toast('Bajarilmoqda...', icon='♻️')
     prepared_data = get_prepared_data()
-    converted_data = get_converted_data(prepared_data)
-    
-    # appending as result to second columnt to our existing table
+    status, data = get_converted_data(prepared_data)
+    if status is False:
+        msg.toast(data, icon='❌')
+        return False
+
+    # # appending as result to second columnt to our existing table
     data_table = st.session_state['data_table']
-    # second_column_name = data_table[1]
-    data_table['Fonetik ko`rinishi'] = converted_data
-    data_table
-    # update_session_state(data_table, 'data_table')
-
+    second_column_name = data_table.columns[1]
+    data_table[second_column_name] = data
+    update_session_state(data_table, 'data_table')
+    global SHOW_INPUT_TABLE
+    SHOW_INPUT_TABLE = False
+    global SHOW_GENERATED_TABLE
+    SHOW_GENERATED_TABLE = True
+    msg.toast('Fonemalr generatsiyasi tayyor', icon='✅')
+    return True
     
-    
-
-
 
 col1, col2 = st.columns([0.8, 0.2])
 with col1:
@@ -65,12 +75,16 @@ with col1:
 with col2:
     st.selectbox("Tilni tanlang", ['English', 'Uzbek', 'Russian'], index=1)
     if st.button("Generate Phonemes", use_container_width=True, type='primary'):
-        generate_phonemes()
-
-if 'data_table' not in st.session_state:
-    df = pd.DataFrame(columns=("Original so'z yoki matn", "Fonetik ko`rinishi"))
-    data_table = st.data_editor(df, num_rows='dynamic', use_container_width=True)
+        status = generate_phonemes()
+        if status is True:
+            ...
+            
+df = pd.DataFrame(columns=("Original so'z yoki matn", "Fonetik ko`rinishi"))
+if SHOW_INPUT_TABLE:
+    data_table = st.data_editor(df, num_rows='dynamic', use_container_width=True, key='input_table')
     update_session_state('data_table', data_table)
 
-# if __name__ == '__main__':
-#     ...
+if SHOW_GENERATED_TABLE:
+    data_table = st.session_state['data_table']
+    data_table = st.data_editor(data_table, num_rows='dynamic', use_container_width=True)
+    st.session_state['data_table'] = data_table
